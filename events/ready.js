8e92e27aecfc80274e91c5afc979ac291bcbc114
BILLYS123-JPG <78544556+BILLYS123-JPG@users.noreply.github.com>
2021-02-11 19:01:03 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("Myprefic is -bg", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
